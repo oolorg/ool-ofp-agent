01e720748d40ecbe4f10b17e7090bfa4f0b5445e
ool-taku <taku.arakaki@oolorg.mygbiz.com>
2014-02-10 18:42:12 +0900

    削除

--- src/main/java/ool/com/ofpm/client/AgentClient.java
+++
@@ -1,9 +0,0 @@
-package ool.com.ofpm.client;
-
-import ool.com.ofpm.json.BaseResultIn;
-
-public interface AgentClient {
-	public BaseResultIn getTopology() throws Exception;
-	public BaseResultIn addFlows() throws Exception;
-	public BaseResultIn delFlows() throws Exception;
-}
