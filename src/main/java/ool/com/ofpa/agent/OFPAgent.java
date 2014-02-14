0ae384301aab8250ed55bede8a3c77400df743b2
ool-matayoshi <junichi.matayoshi@oolorg.mygbiz.com>
2014-02-14 09:43:10 +0900

    encode fix

--- src/main/java/ool/com/ofpa/agent/OFPAgent.java
+++ src/main/java/ool/com/ofpa/agent/OFPAgent.java
@@ -26,7 +26,7 @@ public class OFPAgent extends HttpServlet {
         Gson gson = new Gson();
         OFPResponseOut outPara = new OFPResponseOut();
         try {
-            // json竊丹bject
+            // json→bject
             BufferedReader bufferReaderBody = new BufferedReader(req.getReader());
             String body = bufferReaderBody.readLine();
 
