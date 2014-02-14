bc3c66eef396bf634ea89b65a8914839cec1be6b
m-nakao <masanori.nakao@oolorg.mygbiz.com>
2014-02-14 09:39:18 +0900

    code fix

--- src/main/java/ool/com/ofpa/agent/OFPAgent.java
+++ src/main/java/ool/com/ofpa/agent/OFPAgent.java
@@ -26,7 +26,7 @@ public class OFPAgent extends HttpServlet {
         Gson gson = new Gson();
         OFPResponseOut outPara = new OFPResponseOut();
         try {
-            // json竊丹bject
+            // json->object
             BufferedReader bufferReaderBody = new BufferedReader(req.getReader());
             String body = bufferReaderBody.readLine();
 
